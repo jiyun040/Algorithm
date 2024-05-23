8f051f58885124d58cfc739352644132faec9342
정지윤 <163826760+jiyun040@users.noreply.github.com>
2024-05-23 22:15:47 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.c
+++ 백준/Bronze/2884. 알람 시계/알람 시계.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-    int H = 0;
-    int M = 0;
-    scanf("%d %d", &H, &M);
-    M = M - 45;
-    if (M < 0)
+	int H, M;
+	scanf("%d %d", &H, &M);
+
+	M = M - 45;
+
+	if (M < 0)
     {
-        H -= 1;
-        if (H < 0)
-            H += 24;
-        M += 60;
+		H = H - 1;
+	if (H < 0)
+		H = H + 24;
+	M = M + 60;
     }
-    printf("%d %d", H, M);
-    return 0;
+	printf("%d %d", H, M);
+    
+	return 0;
 }
